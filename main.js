82d65d547897d4bf6cbfef0c6c1dda0ead29d179
Adrian-Moldovan <ma.moldovan@gmail.com>
2023-09-30 09:16:00 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,206 @@
+console.log('main.js loaded...');
+
+let mapDiv = document.querySelector('#map');
+
+
+function drawLineTile(tileType){
+    let tileDiv = document.createElement('div');
+    tileDiv.classList.add('card','line-card');
+    tileDiv.classList.add(tileType + '-card');
+    mapDiv.appendChild(tileDiv);
+
+    return tileDiv;
+}
+
+
+function drawLineCardFooter(parentDiv, text){
+    let cardPrice = document.createElement('div');
+    cardPrice.classList.add('card-price');
+    cardPrice.innerText = text;
+    parentDiv.appendChild(cardPrice);
+}
+
+
+function drawLineCardHeader(tileDiv){
+    let cardHeader = document.createElement('div');
+    cardHeader.classList.add('card-header');
+    tileDiv.appendChild(cardHeader);
+
+    return cardHeader;
+}
+
+
+function drawLineCardBody(tileDiv){
+    let cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    tileDiv.appendChild(cardBody);
+
+    return cardBody;
+}
+
+
+function drawPropertyTile(mapTile){
+    let tileDiv = drawLineTile(mapTile.tileType);
+
+    let cardHeader = drawLineCardHeader(tileDiv);
+    cardHeader.style.backgroundColor = mapTile.color;
+
+    let cardBody = drawLineCardBody(tileDiv);
+    cardBody.innerText = mapTile.name;
+
+    drawLineCardFooter(tileDiv, 'M' + mapTile.price);
+
+    return tileDiv;
+}
+
+
+function drawRailroadTile(mapTile){
+    let tileDiv = drawLineTile(mapTile.tileType);
+
+    let cardHeader = drawLineCardHeader(tileDiv);
+    cardHeader.innerText = mapTile.name;
+
+    let cardBody = drawLineCardBody(tileDiv);
+    cardBody.classList.add('card-icon');
+
+    drawLineCardFooter(tileDiv, 'M' + mapTile.price);
+
+    return tileDiv;
+}
+
+
+function drawUtilityTile(mapTile){
+    let tileDiv = drawLineTile(mapTile.tileType);
+
+    let cardHeader = drawLineCardHeader(tileDiv);
+    cardHeader.innerText = mapTile.name;
+
+    let cardBody = drawLineCardBody(tileDiv);
+    cardBody.classList.add('card-icon', 'utility-' + mapTile.utilityType);
+
+    drawLineCardFooter(tileDiv, 'M' + mapTile.price);
+
+    return tileDiv;
+}
+
+
+function drawTaxTile(mapTile){
+    let tileDiv = drawLineTile(mapTile.tileType);
+
+    let cardHeader = drawLineCardHeader(tileDiv);
+    cardHeader.innerText = mapTile.name;
+
+    let cardBody = drawLineCardBody(tileDiv);
+    cardBody.classList.add('card-icon');
+
+    drawLineCardFooter(tileDiv, 'PAY M' + mapTile.price);
+
+    return tileDiv;
+}
+
+
+function drawGenericLineTile(mapTile){
+    let tileDiv = drawLineTile(mapTile.tileType);
+
+    let cardHeader = drawLineCardHeader(tileDiv);
+    cardHeader.innerText = mapTile.name;
+
+    let cardBody = drawLineCardBody(tileDiv);
+    cardBody.classList.add('card-icon');
+
+    return tileDiv;
+}
+
+
+function drawCornerTile(mapTile){
+    let tileDiv = document.createElement('div');
+    tileDiv.classList.add('card', 'corner-card');
+    tileDiv.classList.add(mapTile.tileType + '-card');
+    mapDiv.appendChild(tileDiv);
+
+    return tileDiv;
+}
+
+
+// draw properties, utilities and map corners
+for (let i = 0; i < mapTiles.length; i++) {
+    let tileDiv;
+    switch (mapTiles[i].tileType) {
+        case 'property':
+            tileDiv = drawPropertyTile(mapTiles[i]);     
+            break;
+
+        case 'railroad':
+            tileDiv = drawRailroadTile(mapTiles[i]);
+            break;  
+
+        case 'utility':
+            tileDiv = drawUtilityTile(mapTiles[i]);
+            break;
+            
+        case 'income-tax':
+        case 'luxury-tax':
+            tileDiv = drawTaxTile(mapTiles[i]);
+            break;
+
+        case 'start':
+        case 'jail':
+        case 'parking':
+        case 'police':
+            tileDiv = drawCornerTile(mapTiles[i]);
+            break;
+
+        default:
+            tileDiv = drawGenericLineTile(mapTiles[i]);
+            break;
+    }
+
+    let rowIndex, colIndex, rotation, top, right;
+
+    if(i >=0 && i < 10){
+        // i:  0 -> 9   (bottom side)
+        rowIndex = 11;
+        colIndex = 11 - i;
+        rotation = 0;
+        top = 0;
+        right = 0;
+    }
+    else if(i >=10 && i < 20){
+        // i:  10 -> 19   (left side)
+        rowIndex = 11 - i + 10;
+        colIndex = 1;
+        rotation = 90;
+        top = -28;
+        right = -28;
+    }
+    else if(i >=20 && i < 30){
+        // i:  20 -> 29   (top side)
+        rowIndex = 1;
+        colIndex = i - 19;
+        rotation = 180;
+        top = 0;
+        right = 0;
+    }
+    else{
+        // i:  30 -> 39   (right side)
+        rowIndex = i - 29; //????
+        colIndex = 11;
+        rotation = 270;
+        top = -28;
+        right = -28;
+    }
+
+    if(i == 0 || i == 10 || i == 20 || i == 30){
+        // the corner cards
+        top = 0;
+        right = 0;
+    }
+
+    tileDiv.style.gridColumn = colIndex;
+    tileDiv.style.gridRow = rowIndex;
+    tileDiv.style.transform = `rotate(${rotation}deg)`;
+    tileDiv.style.top = `${top}px`;
+    tileDiv.style.right = `${right}px`;
+}
+
+// draw map center
---
+++ map/map-tiles.js
@@ -0,0 +1,234 @@
+
+let mapTiles = [
+
+    // --- BOTTOM SIDE ---
+
+    {
+        tileType: "start",
+        name: 'GO',
+        description: 'Collect M200 salary as you go pass',
+        reward: 200
+    },
+    {
+        tileType: "property",
+        name: "MEDITERANEAN AVENUE",
+        price: 60,
+        color: "#955436"
+    },
+    {
+        tileType: "community-chest",
+        name: "COMMUNITY CHEST"
+    },
+    {
+        tileType: "property",
+        name: "BALTIC AVENUE",
+        price: 60,
+        color: "#955436"
+    },
+    {
+        tileType: "income-tax",
+        name: "INCOME TAX",
+        price: 200
+    },
+    {
+        tileType: "railroad",
+        name: "READING RAILROAD",
+        price: 200
+    },
+    {
+        tileType: "property",
+        name: "ORIENTAL AVENUE",
+        price: 100,
+        color: "#aae0fa"
+    },
+    {
+        tileType: "chance",
+        name: "CHANCE",
+        color: "#d93a96"
+    },
+    {
+        tileType: "property",
+        name: "VERMONT AVENUE",
+        price: 100,
+        color: "#aae0fa"
+    },
+    {
+        tileType: "property",
+        name: "CONNECTICUT AVENUE",
+        price: 120,
+        color: "#aae0fa"
+    },
+
+    // --- LEFT SIDE ---
+
+    {
+        tileType: "jail",
+        name: 'IN JAIL'
+    },
+    {
+        tileType: "property",
+        name: "ST. CHARLES PLACE",
+        price: 140,
+        color: "#d93a96"
+    },
+    {
+        tileType: "utility",
+        name: "ELECTRIC COMPANY",
+        price: 150,
+        utilityType: "electricity"
+    },
+    {
+        tileType: "property",
+        name: "STATES AVENUE",
+        price: 140,
+        color: "#d93a96"
+    },
+    {
+        tileType: "property",
+        name: "VIRGINIA AVENUE",
+        price: 160,
+        color: "#d93a96"
+    },
+    {
+        tileType: "railroad",
+        name: "PENNSYLVANIA RAILROAD",
+        price: 200
+    },
+    {
+        tileType: "property",
+        name: "ST. JAMES PLACE",
+        price: 180,
+        color: "orange"
+    },
+    {
+        tileType: "community-chest",
+        name: "COMMUNITY CHEST"
+    },
+    {
+        tileType: "property",
+        name: "TENESSEE AVENUE",
+        price: 180,
+        color: "orange"
+    },
+    {
+        tileType: "property",
+        name: "NEW YORK AVENUE",
+        price: 200,
+        color: "orange"
+    },
+    
+    // --- TOP SIDE ---
+
+    {
+        tileType: "parking",
+        name: 'FREE PARKING'
+    },
+    {
+        tileType: "property",
+        name: "KENTUCKY AVENUE",
+        price: "220",
+        color: "#FF0000"
+    },
+    {
+        tileType: "chance",
+        name: "CHANCE",
+        color: "#0062b0"
+    },
+    {
+        tileType: "property",
+        name: "INDIANA AVENUE",
+        price: "220",
+        color: "#FF0000"
+    },
+    {
+        tileType: "property",
+        name: "ILLINOIS AVENUE",
+        price: "240",
+        color: "#FF0000"
+    },
+    {
+        tileType: "railroad",
+        name: "B. & O.",
+        price: 200
+    },
+    {
+        tileType: "property",
+        name: "ATLANTIC AVENUE",
+        price: 260,
+        color: "#FFFF00",
+    },
+    {
+        tileType: "property",
+        name: "VENTNOR AVENUE",
+        price: 260,
+        color: "#FFFF00"
+    },
+    {
+        tileType: "utility",
+        name: "WATER WORKS",
+        price: 150,
+        utilityType: "water"
+    },
+    {
+        tileType: "property",
+        name: 'MARVIN GARDENS',
+        price: 280,
+        color: "#FFFF00"
+    },
+
+    //   --- RIGHT SIDE ---
+
+    {
+        tileType: "police",
+        name: 'GO TO JAIL'
+    },
+    {
+        tileType: "property",
+        name: 'PACIFIC AVENUE',
+        price: 300,
+        color: 'green'
+    },
+    {
+        tileType: "property",
+        name: 'NORTH CAROLINA AVENUE',
+        price: 300,
+        color: 'green'
+    },
+    {
+        tileType: "community-chest",
+        name: "COMMUNITY CHEST"
+    },
+    {
+        tileType: "property",
+        name: 'PENNSYLVANIA AVENUE',
+        price: 320,
+        color: 'green'
+    },
+    {
+        tileType: "railroad",
+        name: "SHORT LINE",
+        price: 200
+    },
+    {
+        tileType: "chance",
+        name: "CHANCE",
+        color: "orange"
+    },
+    {
+        tileType: "property",
+        name: 'PARK PLACE',
+        price: 350,
+        color: '#0062b0'
+    },
+    {
+        tileType: "luxury-tax",
+        name: "LUXURY TAX",
+        price: 100
+    },
+    {
+        tileType: "property",
+        name: 'BOARDWALK',
+        price: 400,
+        color: '#0062b0'
+    }
+];
