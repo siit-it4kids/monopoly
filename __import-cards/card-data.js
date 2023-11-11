0d8eebfc2117e76d644171e1812249df9cc9e063
Adrian-Moldovan <ma.moldovan@gmail.com>
2023-11-11 10:02:32 +0200

    Adds money property to cards

--- __import-cards/card-data.js
+++ __import-cards/card-data.js
@@ -2,14 +2,17 @@ let data =
 [
     {
         playerKeepsCard: false,
-        message: "ADVANCE TO THE NEAREST RAILROAD STATION IF YOU PASS 'GO' COLLECT 200$"
+        message: "ADVANCE TO THE NEAREST RAILROAD STATION IF YOU PASS 'GO' COLLECT 200$",
+        money: 200
     },
     {
         playerKeepsCard: false,
-        message: "IT'S YOUR LUCKY DAY: ROLL THE DICE AGAIN!"
+        message: "IT'S YOUR LUCKY DAY: ROLL THE DICE AGAIN!",
+        money: 0
     },
     {
         playerKeepsCard: true,
-        message: "You rescue a puppy-and ypu feel rescued, too! \n GET OUT OF JAIL FREE \n Keep this card until needed, traded or sold."
+        message: "You rescue a puppy-and you feel rescued, too! \n GET OUT OF JAIL FREE \n Keep this card until needed, traded or sold.",
+        money: 0
     },
 ]
