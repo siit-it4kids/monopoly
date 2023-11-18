f3c7c712855024864fd74c286a56c1bf72abd529
Adrian-Moldovan <ma.moldovan@gmail.com>
2023-11-18 12:06:07 +0200

    Adds community chest and chance cards

---
+++ js/cards/chace-cards copy.js
@@ -0,0 +1,66 @@
+export default [
+    {
+        message: "Advance to Boardwalk",
+        move: "Boardwalk"
+    },
+    {
+        message: "Advance to Go (Collect $200)",
+        move: "Go"
+    },
+    {
+        message: "Advance to Illinois Avenue. If you pass Go, collect $200",
+        move: "Illinois"
+    },
+    {
+        message: "Advance to St. Charles Place. If you pass Go, collect $200",
+        move: "St. Charles Place"
+    },
+    {
+        message: "Advance to the nearest Railroad. If unowned, you may buy it from the Bank. If owned, pay wonder twice the rental to which they are otherwise entitled",
+        move: "Railroad"
+    },
+    {
+        message: "Advance to the nearest Railroad. If unowned, you may buy it from the Bank. If owned, pay wonder twice the rental to which they are otherwise entitled",
+        move: "Railroad"
+    },
+    {
+        message: "Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times amount thrown",
+        move: "Utility"
+    },
+    {
+        message: "Bank pays you dividend of $50",
+        move: null
+    },
+    {
+        message: "Get Out of Jail Free",
+        move: null
+    },
+    {
+        message: "Go Back 3 Spaces",
+        move: -3
+    },
+    {
+        message: "Go to Jail. Go directly to Jail, do not pass Go, do not collect $200",
+        move: "Jail"
+    },
+    {
+        message: "Make general repairs on all your property. For each house pay $25. For each hotel pay $100",
+        move: null
+    },
+    {
+        message: "Speeding fine $15",
+        move: null
+    },
+    {
+        message: "Make a trip to Reading Railroad. If you pass Go, collect $200",
+        move: "Reading Railroad"
+    },
+    {
+        message: "You have been elected Chairman of the Board. Pay each player $50",
+        move: null
+    },
+    {
+        message: "Your building loan matures. Collect $150",
+        move: null
+    }
+];
---
+++ js/cards/community-chest-cards.js
@@ -0,0 +1,117 @@
+export default [
+    {
+        message: "Advance to Go (Collect $200)",
+        money: 200,
+        from: "bank",
+        keepCard: false,
+        move: "start"
+    },
+    {
+        message: "Bank error in your favor. Collect $200",
+        money: 200,
+        from: "bank",
+        keepCard: false,
+        move: null
+    },
+    {
+        message: "Doctor’s fee. Pay $50",
+        money: -50,
+        from: "own",
+        keepCard: false,
+        move: null
+    },
+    {
+        message: "From sale of stock you get $50",
+        money: 50,
+        from: "bank",
+        keepCard: false,
+        move: null
+    },
+    {
+        message: "Get Out of Jail Free",
+        money: 0,
+        from: null,
+        keepCard: true,
+        move: null
+    },
+    {
+        message: "Go to Jail. Go directly to jail, do not pass Go, do not collect $200",
+        money: 0,
+        from: null,
+        keepCard: false,
+        move: "jail"
+    },
+    {
+        message: "Holiday fund matures. Receive $100",
+        money: 100,
+        from: "bank",
+        keepCard: false,
+        move: null
+    },
+    {
+        message: "Income tax refund. Collect $20",
+        money: 20,
+        from: "bank",
+        keepCard: false,
+        move: null
+    },
+    {
+        message: "It is your birthday. Collect $10 from every player",
+        money: 10,
+        from: "allPlayers",
+        keepCard: false,
+        move: null
+    },
+    {
+        message: "Life insurance matures. Collect $100",
+        money: 100,
+        from: "bank",
+        keepCard: false,
+        move: null
+    },
+    {
+        message: "Pay hospital fees of $100",
+        money: -100,
+        from: "own",
+        keepCard: false,
+        move: null
+    },
+    {
+        message: "Pay school fees of $50",
+        money: -50,
+        from: "own",
+        keepCard: false,
+        move: null
+    },
+    {
+        message: "Receive $25 consultancy fee",
+        money: 25,
+        from: "bank",
+        keepCard: false,
+        move: null
+    },
+    {
+        message: "You are assessed for street repair. $40 per house. $115 per hotel",
+        calculationFormula: {
+            perHouse: -40,
+            perHotel: -115
+        },
+        from: "own",
+        keepCard: false,
+        move: null
+    },
+    {
+        message: "You have won second prize in a beauty contest. Collect $10",
+        money: 10,
+        from: "bank",
+        keepCard: false,
+        move: null
+    },
+    {
+        message: "You inherit $100",
+        money: 100,
+        from: "bank",
+        keepCard: false,
+        move: null
+    },
+];
