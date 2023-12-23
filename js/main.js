7a2bc4f583c1cc18963811ad1e80f689012c4dc5
Mihai <mihai@Mihais-MacBook-Air.local>
2023-12-23 18:03:07 +0200

    Added a close button and made the cards appear when the deck is clicked

--- js/main.js
+++ js/main.js
@@ -6,3 +6,37 @@ console.log('main.js loaded...');
 // draw map
 let mapDiv = document.querySelector('#map');
 drawMap(mapDiv);
+
+let popupBackground = document.querySelector('.popup-container');
+let cardBackground = document.querySelector('.card-back');
+let cardTitle = document.querySelector('.card-title');
+let cardMessage = document.querySelector('.card-message');
+let closeBtn = document.querySelector('.close-btn');
+let chanceCardDeck = document.querySelector('#chance');
+let communityChestCardDeck = document.querySelector('#community-chest');
+
+closeBtn.addEventListener('click', closeCard);
+chanceCardDeck.addEventListener('click', showChanceCard);
+communityChestCardDeck.addEventListener('click', showCommunityChestCard);
+
+function closeCard(){
+    popupBackground.style.display = "none";
+}
+
+function showCommunityChestCard()
+{   
+    popupBackground.style.display = "flex";
+    cardTitle.innerText = 'Community Chest Card';
+    cardMessage.innerText = getCommunityChestCard().message;
+}
+
+showCommunityChestCard();
+
+function showChanceCard()
+{
+    popupBackground.style.display = "flex";
+    cardTitle.innerText = 'Chance Card';
+    cardMessage.innerText = getChanceCard().message;
+}
+
+showChanceCard();
