df207ea5c79200285e6a2afe86e7bd01dbcc22f4
Adrian-Moldovan <ma.moldovan@gmail.com>
2024-01-20 10:49:06 +0200

    adds players

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { drawMap } from "./map/map.js";
 import { cardsPopupInit } from "./popups/card-popup.js";
+import players from "./players/players.js"
 
 console.log('main.js loaded...');
 
@@ -9,3 +10,5 @@ drawMap(mapDiv);
 
 // load chance cards and community chest card behaviour
 cardsPopupInit();
+
+console.log(players);
